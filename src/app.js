d0f88ff46627446e567b5aade7629e41f7bf9516
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-05-04 21:26:35 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import AppRouter, { history } from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { login, logout } from './actions/auth'
+import { firebase } from './firebase/firebase'
+import LoadingPage from './components/LoadingPage'
+
+import 'normalize.css/normalize.css'
+import 'react-dates/lib/css/_datepicker.css';
+import './styles/styles.scss'
+
+const store = configureStore()
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+let hasRendered = false
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.querySelector('#app'))
+        hasRendered = true
+    }
+}
+
+ReactDOM.render(<LoadingPage />, document.querySelector('#app'))
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid))
+        renderApp()
+        if (history.location.pathname === '/') {
+            history.push('/dashboard')
+        }
+    } else {
+        store.dispatch(logout())
+        renderApp()
+        history.push('/')
+    }
+})
